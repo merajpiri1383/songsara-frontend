185f92f0a70b00c49da0470fbe1f1c8036991e32
meraj <merajpiri83@gmail.com>
2024-07-26 22:30:41 +0330

    add albums scroller in home page

---
+++ app/components/albums.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useMemo, useState } from "react";
+import Loading from "./loading";
+import API from "../../src/api";
+
+export default function Albums() {
+    const [showLoading, setShowLoading] = useState(true);
+    const [albums, setAlbums] = useState([]);
+    const getData = async () => {
+        await API.get("/album/").then((response) => {
+            setAlbums(response.data.filter((item, index) => {
+                return index < 16;
+            }));
+            setTimeout(() => setShowLoading(false), 400);
+        }).catch((error) => error.response && error.response.status === 401 && getData());
+    };
+    useMemo(() => getData(), []);
+
+    return (
+        <>
+            {
+                showLoading && <Loading />
+            }
+            {
+                !showLoading && <>
+                    <div className="albums p-2 mx-4 my-6">
+                        {
+                            albums.map((album, index) => {
+                                return (
+                                    <div key={index} className="album hover:bg-gray-700 cursor-pointer transition">
+                                        <img
+                                            src={album.image}
+                                            alt={album.name}
+                                            className="h-60 w-56 object-cover"
+                                        />
+                                        <h3 className="text-left text-lg font-semibold text-white px-2 my-1 w-56 h-6">{album.name}</h3>
+                                        <p className="text-amber-400 text-left w-56 px-2 my-1">{album.artist.name}</p>
+                                        <p className="text-gray-200 text-left w-56 px-2 my-1">{album.genre.name}</p>
+                                        <p className="text-gray-400 text-left w-56 px-2 my-1">{album.created_date}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </>
+            }
+        </>
+    )
+};
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import { lazy } from "react";
 const Mood = lazy(() => import("./components/mood"));
+const Albums = lazy(() => import("./components/albums"));
 
-export default function Page () {
+export default function Page() {
   return (
     <>
-    <Mood />
+      <Mood />
+      <Albums />
     </>
   )
 };
